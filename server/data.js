b66ba5c46661d7fc00c6c33a33e7d19385ba2bf0
rushikesh garad <garadrushikesh042@gmail.com>
2025-08-16 12:35:13 +0530

    Update data.js

--- server/data.js
+++ server/data.js
@@ -3,10 +3,10 @@ let util = require('util')
 
 
 let conn = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'tutionclass'
+    host:'bfzrsc6p2jgqojbyksvj-mysql.services.clever-cloud.com',
+    user:'uudfmprfthahkqmp',
+    password:'3vQYNjeOE8mX45DPgSeE',
+    database:'bfzrsc6p2jgqojbyksvj'
 })
 
 let exe = util.promisify(conn.query).bind(conn)
