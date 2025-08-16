11eacc38ee79577df616762b9524b8d599c6278d
rushikesh garad <garadrushikesh042@gmail.com>
2025-08-16 12:38:18 +0530

    Update data.js

--- server/data.js
+++ server/data.js
@@ -1,15 +1,15 @@
-let mysql = require('mysql')
-let util = require('util')
-
+let mysql = require("mysql");
+let util = require("util");
 
 let conn = mysql.createConnection({
-    host:'bfzrsc6p2jgqojbyksvj-mysql.services.clever-cloud.com',
-    user:'uudfmprfthahkqmp',
-    password:'3vQYNjeOE8mX45DPgSeE',
-    database:'bfzrsc6p2jgqojbyksvj'
-})
+  host: process.env.MYSQL_ADDON_HOST,
+  port: process.env.MYSQL_ADDON_PORT || 3306,   // Clever Cloud gives you exact port
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: true } : undefined
+});
 
-let exe = util.promisify(conn.query).bind(conn)
+let exe = util.promisify(conn.query).bind(conn);
 
-
-module.exports = exe
+module.exports = exe;
